8a6c013f892748ae6c3b2ff971ec73717f90f0b7
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-08-27 20:39:18 +0200

    Update Song+videos.js

--- plugins/Song+videos.js
+++ plugins/Song+videos.js
@@ -44,3 +44,55 @@ console.log(e)
 reply('${e}')
 }    
 })
+
+
+
+=============video-dl===============
+    
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply ("please give me url lr title")    
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+    ⭐*WATSON-MD VIDEO DOWNLOADER*⭐
+    
+title: ${data.title}    
+description: ${data.description}    
+time: ${data.timestamp}    
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY WATSON-XD🦋
+` 
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});    
+
+//download videos
+
+let down = await fg.yta(url)
+let downloadurl = down.dl_url 
+
+//send video message 
+await conn.sendMessage(from,{video: {url:downloadurl},mimetype:"video/mp4"},{quoted:mek}) 
+await conn.sendMessage(from,{document: {url:downloadurl},mimetype:"video/mp4",FileName:data.title + ".mp4"},{quoted:mek}) 
+
+
+
+}catch(e){
+console.log(e)
+reply('${e}')
+}    
+})
+
+    
+    
+    
+    
