46dfdfe3c6b431b312da281c9ea88b3e9a2d9f4b
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-08-27 23:04:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,8 @@ mek.type === "stickerMessage"
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
 //============================================================================ 
-
+if(!isowner && config.MODE === "private") return 
+if(!isowner && is group && config.MODE === "inbox") return 
 })
 }
 app.get("/", (req, res) => {
