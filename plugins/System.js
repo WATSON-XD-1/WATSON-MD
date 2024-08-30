e6545983eb00a10fa92e913c203d4ed599f5f6a9
WATSON-XD-1 <watsonfourpence209@gmail.com>
2024-08-30 10:53:22 +0200

    Update System.js

--- plugins/System.js
+++ plugins/System.js
@@ -11,10 +11,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryusage().heapused / 1024 / 1024).toFixed(2)MB / ${Math.round(require('os')totalmem / 1024 / 1024)}MB                               
-*Hostname:* ${(os.hostname()}
-*Owner:* Prabath kumara && watson-xd
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* Prabath Kumara & watson-md 
 ` 
 return reply (`${status}`)
 }catch(e){
